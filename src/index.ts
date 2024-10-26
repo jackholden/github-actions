73e64de3230a62a6456ac3a6dd4e43b68344ba11
jack <75576058+jackholden@users.noreply.github.com>
2024-10-26 15:58:42 +0100

    feat: add maths functions (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+const add = (a: number, b: number) => a + b;
+const subtract = (a: number, b: number) => a - b;
+const multiply = (a: number, b: number) => a * b;
+const divide = (a: number, b: number) => a / b;
+
+export { add, subtract, multiply, divide };
