6292be9bc641a53a9eba9da97943a035d73ebb19
Jack Holden <75576058+jackholden@users.noreply.github.com>
2024-10-26 16:00:39 +0100

    chore: remove comment

--- src/maths.ts
+++ src/maths.ts
@@ -6,7 +6,6 @@ const divide = (a: number, b: number) => a / b;
 const string = (a: string, b: string) => {
   if (process.env.NEW_FEATUREA === "false") {
     return undefined;
-    // or return null/undefined depending on your needs
   }
 
   return `${a} and ${b}`;
